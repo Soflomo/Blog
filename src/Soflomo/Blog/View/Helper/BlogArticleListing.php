7fa9433b05dd49c02487a3c1aa059fb1a323a56b
Jurian Sluiman <jurian@soflomo.com>
2013-06-10 14:21:54 +0200

    Add view helper for article listing

---
+++ src/Soflomo/Blog/View/Helper/BlogArticleListing.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Copyright (c) 2013 Jurian Sluiman.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of the
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author      Jurian Sluiman <jurian@soflomo.com>
+ * @copyright   2013 Jurian Sluiman.
+ * @license     http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link        http://soflomo.com
+ */
+
+namespace Soflomo\Blog\View\Helper;
+
+use Zend\View\Helper\AbstractHelper;
+use Doctrine\ORM\EntityRepository   as BlogRepository;
+use Soflomo\Blog\Repository\Article as ArticleRepository;
+use Soflomo\Blog\Exception;
+
+class BlogArticleListing extends AbstractHelper
+{
+    const DEFAULT_ARTICLE_LIMIT = 10;
+
+    protected $repository;
+
+    public function __construct(BlogRepository $blogRepository, ArticleRepository $articleRepository)
+    {
+        $this->blogRepository    = $blogRepository;
+        $this->articleRepository = $articleRepository;
+    }
+
+    public function __invoke($blog, $limit = null)
+    {
+        $blog  = $this->getBlog($blog);
+        $limit = $limit ?: self::DEFAULT_ARTICLE_LIMIT;
+
+        return $this->getArticleRepository()->findRecent($blog, $limit);
+    }
+
+    protected function getBlog($idOrSlug)
+    {
+        if (is_int($idOrSlug)) {
+            $blog = $this->getBlogRepository()->find($idOrSlug);
+        } else {
+            $blog = $this->getBlogRepository()->findOneBySlug($idOrSlug);
+        }
+
+        if (null === $blog) {
+            throw new Exception\BlogNotFoundException(sprintf(
+                'Blog with slug "%s" not found', $slug
+            ));
+        }
+
+        return $blog;
+    }
+
+    protected function getBlogRepository()
+    {
+        return $this->blogRepository;
+    }
+
+    protected function getArticleRepository()
+    {
+        return $this->articleRepository;
+    }
+}
